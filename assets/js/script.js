33a743528aecaa2bdc07054d1c587079710c2243
TatiGarrido <tatieeegarrido@hotmail.com>
2022-10-17 18:27:55 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,43 @@
-//wait for the DOM finish loading before running the game
-//Get the button element and add event listeners to them
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons){
+    for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
-        })
+        });
     }
-})
+
+    runGame("addition");
+
+});
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(){
-// create two random numbers between 1 and 25 (o +1 faz com que o zero seja excluido)
+function runGame(gameType) {
+
+    // Creates two random numbers between 1 and 25 (o +1 faz com que o zero seja excluido)
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
+
 
 function checkAnswer(){
 
@@ -42,8 +55,12 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
+    
 }
 
 function displaySubtractQuestion(){
@@ -53,3 +70,4 @@ function displaySubtractQuestion(){
 function displayMultiplyQuestion(){
 
 }
+
