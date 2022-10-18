6c5af29b6d906a3961ae4fa92d58e92138542dd9
TatiGarrido <tatieeegarrido@hotmail.com>
2022-10-18 19:59:08 +0000

    divion code added to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,8 @@ function runGame(gameType) {
        displayMultiplyQuestion(num1, num2); 
     } else if (gameType === "subtract"){
         displaySubtractQuestion(num1, num2);
+    } else if(gameType === "division"){
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -84,7 +86,9 @@ function calculateCorrectAnswer(){
         return[operand1 * operand2, "multiply"];
     } else if(operator === "-"){
         return[operand1 - operand2, "subtract"];
-    }else {
+    } else if (operator === "/"){
+        return[operand1 / operand2, "division"];
+    } else {
         alert(`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}.Aborting!`;
     }
@@ -133,8 +137,11 @@ function displayMultiplyQuestion(operand1, operand2){
 
 }
 
-function displayDivisionQuestion(){
+function displayDivisionQuestion(operand1, operand2){
+   operand1 = operand1 * operand2;
 
-
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "/";
 }
 
